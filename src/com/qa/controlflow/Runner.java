9aca28352b32c4e03fbd655f3b0fe6fa2a014482
M-java86 <mpythondata@outlook.com>
2021-05-19 22:52:22 -0400

    MO: added notes to break down flow and function of the code.

--- src/com/qa/controlflow/Runner.java
+++ src/com/qa/controlflow/Runner.java
@@ -1,12 +1,16 @@
 package com.qa.controlflow;
 
 public class Runner {
-
-		public static String msg = "hello!";
+		//Class scope (members)
+		
+	// field/member - attribute (class level variable)	
+	public static String msg = "hello!";
 	
+		//member-Methods/Behavior
 	public static void main(String[] arg) {
 		// TODO Auto-generated method stub
 		
+		//start();
 		System.out.println(msg);
 		start();
 
@@ -19,4 +23,9 @@ public class Runner {
 		System.out.println("This is my Starting Message");
 		
 	}
-}
+}	//end class scope 
+
+
+// Class
+	// attributes (Class members)	
+	// methods (class members)
