d262fbe580e8c56c35c5c2d26348d5afdcebfadf
M-java86 <mpythondata@outlook.com>
2021-05-19 22:30:45 -0400

    MO: added public static String msg;

--- src/com/qa/controlflow/Runner.java
+++ src/com/qa/controlflow/Runner.java
@@ -2,9 +2,12 @@ package com.qa.controlflow;
 
 public class Runner {
 
-	public static void main(String[] args) {
+		public static String msg = "hello!";
+	
+	public static void main(String[] arg) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+		
+		System.out.println(msg);
 
 	}
 
