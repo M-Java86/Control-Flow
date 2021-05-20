d25500d9ecb839dc17838f82c782046c56276043
M-java86 <mpythondata@outlook.com>
2021-05-20 13:55:01 -0400

    MO: modified Animal.java and Runner.java

--- src/com/qa/controlflow/Runner.java
+++ src/com/qa/controlflow/Runner.java
@@ -5,14 +5,20 @@ import com.qa.controlflow.oop.inheritance.Animal;
 public class Runner {
 	
 	
+	
 	public static void main(String[]  args) {
 		Animal a = new Duck ("Mammal", "UK");
 		Animal a2 = new Duck("Mamma1", "UK");
 		
 		System.out.println(a.equals(a.getSpecies());
 		System.out.println(a.getorigin());
+		
+		//everything in java is -a object
+		// - everything inherits from object
+		
+		System.out.println(a.equals(a2));
 	}
-   
+	
 }
 		
 		
--- src/com/qa/controlflow/oop/inheritance/Animal.java
+++ src/com/qa/controlflow/oop/inheritance/Animal.java
@@ -1,5 +1,27 @@
 package com.qa.controlflow.oop.inheritance;
 
 public class Animal {
+	
+	protected String species;
+	private String origin;
+	
+	public Animal() {
+		this("Unkown", "Unknown");
+		
+	}
+	public Animal(String species, String origin) {
+		this.species = species;
+		this.origin = origin;
+		
+	}
+	
+	public String getSpecies() {
+		return species;
+	}
+	
+	public void setSpecies(String species) {
+		this.species = species;
+	}
+}	
 
-}
+
