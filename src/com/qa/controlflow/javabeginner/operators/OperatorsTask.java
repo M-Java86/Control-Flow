3d7b7f52dcbdfc4df59eef0ad71d51aa15ade1dc
M-java86 <mpythondata@outlook.com>
2021-05-22 17:37:59 -0400

    MO: Operators-in class walk through

--- src/com/qa/controlflow/javabeginner/operators/OperatorsTask.java
+++ src/com/qa/controlflow/javabeginner/operators/OperatorsTask.java
@@ -1,13 +1,27 @@
 package com.qa.controlflow.javabeginner.operators;
 
-public class OperatorsTask {
+// Operators are symbols that allow us to perform mathematical operations 
+// + -Addition
+//'-' -Subtraction
+// * -Multiplication
+// '/' -Division
+// '%' -Modulus
 
-	public static void startExercises() {
-		
-		//int variableNumber =12;
-	}
+public class OperatorsTask {
 	
-	public static void task1() {
+	public static void main(String[]  args) {
+		int result; 
 		
+		result = 3 + 10;
+		
+		
+		System.out.println(result);
+		
+	}
+
+	
+	public static void printTthese(int num1, int num2) {
+		System.out.println(num1);
 	}
 }
+	
