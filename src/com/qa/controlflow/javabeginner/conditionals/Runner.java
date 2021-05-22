21d9ff58b405fab74ff9a34e0c626c904541ee02
M-java86 <mpythondata@outlook.com>
2021-05-22 19:40:29 -0400

    MO: Conditionals- In class Follow along

---
+++ src/com/qa/controlflow/javabeginner/conditionals/Runner.java
@@ -0,0 +1,16 @@
+package com.qa.controlflow.javabeginner.conditionals;
+
+// class has a scope
+public class Runner {
+	
+	// Member - attributes
+	public static int id;
+	public static String msg = "Hello Wolrd";
+	
+	//Member - Method has a scope 
+	public static void main(String[] args) {
+		
+	}
+	
+
+}
