d21b8dd87336d586a970290bbfd18860d72e2003
M-java86 <mpythondata@outlook.com>
2021-05-22 19:56:45 -0400

    MO: added if scope and notes

--- src/com/qa/controlflow/javabeginner/conditionals/Runner.java
+++ src/com/qa/controlflow/javabeginner/conditionals/Runner.java
@@ -6,11 +6,26 @@ public class Runner {
 	// Member - attributes
 	public static int id;
 	public static String msg = "Hello Wolrd";
+	public static double passMark = 70%;
 	
 	//Member - Method has a scope 
 	public static void main(String[] args) {
+		double percent = 80.0;
 		
+		if(percent == 80.0) { //the "if" keyword in java is accompanied with its own brackets and parenthesis, also
+			//Can't use = here because it's basically saying that passMark is = to 80. == comparison operator	
+			// it included it's own scope. 
+			
+			System.out.println(percent + "%");
+	
+		
+		
+		System.out.println("You have passed!");
 	}
 	
+	
+	
 
+}
+	
 }
