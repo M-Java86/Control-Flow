432d2dd2ab8690c4f2401b23df19e43eddee7971
M-java86 <mpythondata@outlook.com>
2021-05-22 20:54:56 -0400

    MO: added another "if else" block

--- src/com/qa/controlflow/javabeginner/conditionals/Runner.java
+++ src/com/qa/controlflow/javabeginner/conditionals/Runner.java
@@ -6,25 +6,26 @@ public class Runner {
 	// Member - attributes
 	public static int id;
 	public static String msg = "Hello Wolrd";
-	static double passMark = 75.5;
+	static double passMark = 70;
 	
 	//Member - Method has a scope 
 	public static void main(String[] args) {
-		double percent = 65.5;
+		double percent = 70.0;
 		
 		System.out.println(percent + "%");
 		
-		if(percent >= passMark) {
+		if(percent > passMark) {
 			System.out.println("You have passed it!");
-		} else {
+		} else if (percent == passMark) { 
+			System.out.print("You have JUST passed!");
+		}   else {
 			System.out.println("You have failure!");
+		
 		}
 		
 	}
+	
 }
-
-
-		
 			
 		
 //the "if" keyword in java is accompanied with its own brackets and parenthesis, also
